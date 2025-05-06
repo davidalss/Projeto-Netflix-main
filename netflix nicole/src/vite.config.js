8e31c9d0ec5ee5c3a7845f73a362d4493d30a84d
David Alisson <david.alisson92@Outlook.com>
2025-05-06 01:00:15 -0300

    bug

---
+++ netflix nicole/src/vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+export default {
+    build: {
+      outDir: 'dist', // Diretório de saída onde a build será gerada
+    },
+    server: {
+      port: 3000, // Porta do servidor local para desenvolvimento
+    },
+  };
+  
